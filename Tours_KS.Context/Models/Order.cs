6150063d2398788d96e6e08bd8d075b8bbcd2431
Елена Лебедева <Kristina.Smirnova1310@yandex.ru>
2024-01-29 10:28:45 +0300

    Миграция User

--- Tours_KS.Context/Models/Order.cs
+++ Tours_KS.Context/Models/Order.cs
@@ -9,10 +9,10 @@ namespace Tours_KS.Context.Models
     public class Order
     {
         public int Id { get; set; }
+
+        public int UserId { get; set; }
         
-        public User UserId { get; set; }
-        
-        public string User { get; set; }
+        public User User { get; set; }
         
         public decimal Price { get; set; }
         
